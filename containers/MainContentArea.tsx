e0604b71ac365418a4d680a9ab69e9d44f834a61
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-07-18 20:35:06 +0700

    fix redirect error for not having layout in app

--- containers/MainContentArea.tsx
+++ containers/MainContentArea.tsx
@@ -1,24 +1,71 @@
-import React, { useEffect } from "react";
-import { socket } from "@/utils/socketClient";
+"use client";
+import React, { FormEvent, useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import {
+  disconnectFromSocketServer,
+  handleGroupJoinEvent,
+  connectToSocketServer,
+  handleIncomingMessage,
+} from "@/services/socketService";
+import { RootState } from "@/store";
+import { handleSendMessage } from "@/services/socketService";
 
 const MainContentArea = () => {
+  const { activeRoom } = useSelector((state: RootState) => state.chatRoom);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [message, setMessage] = useState("");
+
+  const handleGroupJoin = async (groupId: number) => {
+    console.log("xxx handleSocket");
+    handleGroupJoinEvent(groupId);
+  };
+
+  console.log("xxx activeRoom ", activeRoom);
+
+  const handleMessage = () => {
+    setMessage(message);
+  };
+
   useEffect(() => {
-    socket.on("connect", () => {
-      console.log("Connected to Socket.IO server");
-    });
-  }, []);
+    if (activeRoom) {
+      // connectToSocketServer();
+      // handleGroupJoinEvent(activeRoom.id);
+      handleSendMessage({
+        groupId: activeRoom.id,
+        content: "No Message",
+        receiverId: activeRoom?.members?.[0].id,
+      });
+    }
+    // handleIncomingMessage();
+  }, [activeRoom]);
+
+  const onSendMessage = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (activeRoom?.members && activeRoom?.members.length > 0) {
+      console.log("xxx onSendMessage ", activeRoom?.members[0]?.id);
+    }
+  };
+
   return (
     <div className="bg-middle flex-grow flex flex-col relative">
       <div className="flex-grow p-4 overflow-y-auto">
-        {/**Chat content will go here   */}
+        <h1 className="text-white text-xl"> {message} </h1>
       </div>
       <div className="w-[80%] mx-auto mb-10">
-        <input
-          type="text"
-          id="promptInput"
-          className="w-full p-4 border border-[#303139] rounded bg-[#40414E] focus:outline-none text-white"
-          placeholder="Type your message here..."
-        />
+        <div className="flex">
+          <input
+            ref={inputRef}
+            type="text"
+            id="promptInput"
+            className="w-full p-4 border border-[#303139] rounded bg-[#40414E] focus:outline-none text-white"
+            placeholder="Type your message here..."
+          />
+          <button className="p-2 bg-accGreen  rounded" onClick={onSendMessage}>
+            {" "}
+            Send{" "}
+          </button>
+        </div>
+
         <p className="text-grayFP">
           ChatGPT Jan 9 Version. Free Research Preview. Our goal is to make AI
           systems more natural and safe to interact with. Your feedback will
