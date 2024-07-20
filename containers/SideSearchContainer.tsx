8cbac1defd7eb9da4fdeb73951ffa23cd9cfa0f9
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-07-20 12:55:33 +0700

    handle search and create group action

--- containers/SideSearchContainer.tsx
+++ containers/SideSearchContainer.tsx
@@ -4,10 +4,17 @@ import { userListFetcher } from "@/services/userService";
 import { updateActiveRoom } from "@/store/ChatRoomReducer";
 import { useDispatch } from "react-redux";
 import { createChatRoom } from "@/services/chatRoomService";
+import { IoMdCheckmark } from "react-icons/io";
 
-const SideSearchContainer = () => {
+type SideSearchContainerProps = {
+  onSearchHandle: (status: boolean) => void;
+  friendListMutate: any
+};
+
+const SideSearchContainer = ({ onSearchHandle, friendListMutate }: SideSearchContainerProps) => {
   const [inputValue, setInputValue] = useState("");
   const [friendList, setFriendList] = useState([]);
+  const [selectedFriends, setSelectedFriends] = useState<number[]>([]);
   const dispatch = useDispatch();
 
   const handleFriendList = async () => {
@@ -25,17 +32,16 @@ const SideSearchContainer = () => {
     handleFriendList();
   }, [inputValue]);
 
-  const handleUserItemClick = async (user: {
-    id: number;
-    name?: string;
-    email: string;
-  }) => {
+  const handleCreateRoom = async () => {
     const res = await createChatRoom({
-      name: `Group with ${user?.name || user.email}`,
-      members: [user.id],
+      name: `Group with ${selectedFriends?.join(",")}`,
+      members: selectedFriends,
     });
     if (res.success) {
       dispatch(updateActiveRoom(res.data));
+      friendListMutate()
+      onSearchHandle(false);
+
     }
   };
 
@@ -48,20 +54,47 @@ const SideSearchContainer = () => {
         className="w-full p-4 border border-[#303139] rounded bg-[#40414E] focus:outline-none text-white"
         placeholder="Type something..."
       />
-      <div className="flex-grow overflow-y-auto">
-        <ul id="chatHistory" className="p-2 space-y-2">
-          {friendList.map(
-            (user: { id: number; name?: string; email: string }) => (
-              <li
-                onClick={(id) => handleUserItemClick(user)}
-                className="p-2 bg-grayIc rounded cursor-pointer hover:bg-gray-600 truncate"
-                key={user.id}>
-                {user?.name || user.email}
-              </li>
-            )
-          )}
-        </ul>
-      </div>
+      {friendList?.length > 0 && (
+        <>
+          <div className="flex-grow overflow-y-auto">
+            <ul id="chatHistory" className="p-2 space-y-2">
+              {friendList.map(
+                (user: { id: number; name?: string; email: string }) => (
+                  <li
+                    onClick={() => {
+                      setSelectedFriends((friends) =>
+                        friends.includes(user.id)
+                          ? friends.filter((f) => f !== user.id)
+                          : [...friends, user.id]
+                      );
+                    }}
+                    className="flex justify-between p-2 bg-gray-600 rounded cursor-pointer"
+                    key={user.id}>
+                    <div className="w-4/5 truncate">
+                      {user?.name || user.email}
+                    </div>
+                    {selectedFriends.includes(user.id) && <IoMdCheckmark />}
+                  </li>
+                )
+              )}
+            </ul>
+          </div>
+          <div className="flex justify-between px-4">
+            <button
+              type="button"
+              className="p-2 border border-accGreen rounded"
+              onClick={() => onSearchHandle(false)}>
+              Cancel
+            </button>
+            <button
+              className="p-2 bg-accGreen rounded"
+              onClick={handleCreateRoom}
+              disabled={selectedFriends.length <= 0}>
+              Create
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
