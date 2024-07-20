8dba8c03bda673442f160556a52f0f4ce5fec0f8
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-07-20 20:20:36 +0700

    update UI

--- components/Buttons/OAuthButton.tsx
+++ components/Buttons/OAuthButton.tsx
@@ -13,8 +13,8 @@ const OAuthButton = ({
   image: string;
 }) => {
   return (
-    <Link href={link} className="my-2 w-full h-full">
-      <button className="w-full flex gap-4 items-center p-2 border border-[#C3C8CF] text-[#2E3339] rounded">
+    <Link href={link} className="my-2 w-full h-full text-md">
+      <button className="w-full flex gap-6 items-center p-3 border border-[#C3C8CF] text-[#2E3339] rounded">
         <Image src={image} width="20" height="20" alt="logo" />
         {label}
       </button>
